87687e2a089bb2c1ff9507627c44550ca78179ed
martian2001 <martian2001.as@gmail.com>
2023-06-15 15:24:48 +0530

    Final Code Push

---
+++ resources/views/recapemail.blade.php
@@ -0,0 +1,5 @@
+<div style="font-family: sans-serif;">
+    <p>Here is a quick recape of the application.</p>
+    <p>PostCount: <strong>{{$postCount}}</strong></p>
+    <p>UserCount: <strong>{{$userCount}}</strong></p>
+</div>
